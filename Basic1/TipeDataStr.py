2ece9737a62cb9beb9be5a01552022254023aee1
Agus Sugiono <onowdev@gmail.com>
2022-02-07 08:49:18 +0700

    Squence string

---
+++ Basic1/TipeDataStr.py
@@ -0,0 +1,25 @@
+from ast import Str
+
+
+print("Hello World") # ini adalah tipe data String
+
+#mengubah tipe data lain ke tipe String
+# tipe data lain => tipe data str Gunakan fungsi str(Tipe data Awal)
+
+a = 7
+print(type(a)) 
+
+a = str(7)
+print(type(a))
+
+b = 9.8989
+print(type(b))
+
+b = str(9.8989)
+print(type(b))
+
+c = [1,'buku']
+print(type(c))
+
+c = str([1,'buku'])
+print(type(c))
