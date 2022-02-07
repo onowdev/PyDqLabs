dc84fb8a01c9c48847b6cc2816342896e9ba19fa
Agus Sugiono <onowdev@gmail.com>
2022-02-07 10:46:06 +0700

    if with Else condition

---
+++ Basic1/IF/IfElse.py
@@ -0,0 +1,12 @@
+i = 10 # inisialisasi Variabel i yang memiliki nilai 10
+
+if(i==10): #Pengecekan nilai i apakah sama dengan 10
+    print("Ini adalah angka 10") # Jika TRUE Maka akan mencetak kalimat ini
+else:
+    print("Bukan angka 10") #Jika FALSE akan mencetak kalimat ini
+
+j = 80
+if(j==90):
+    print("True")
+else:
+    print("False")
