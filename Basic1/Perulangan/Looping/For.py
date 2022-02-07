290c433fac0d609fe06e9cf4c1a4abe2ef415920
Agus Sugiono <onowdev@gmail.com>
2022-02-07 12:59:57 +0700

    Looping For

---
+++ Basic1/Perulangan/Looping/For.py
@@ -0,0 +1,17 @@
+'''
+Struktur looping for pada python berbeda dengan struktur for pada umumnya.
+
+for i in range (1,6): #perulangan for sebagai inisialisasi dari angka 1 hingga angka yang lebih kecil daripada 6.
+
+    print("Ini adalah perulangan ke -", i) #perintah jika looping akan tetap berjalan
+
+Maksud dari fungsi for i in range (1,6) adalah, 
+jika kita konversi pada JAVA atau C sama dengan 
+for(i=1;i<6i++). Jika dikonversi menjadi kalimat 
+maka akan menjadi “perulangan dimulai dari nilai i = 1 
+hingga nilai i kurang dari 6 dimana setiap kali perulangan 
+nilai i akan selalu ditambah 1”. Jika nilai i sudah mencapai 
+6 perulangan akan dihentikan.
+'''
+for i in range(1,6):
+    print("ini adalah Perulangan ke -",i)
