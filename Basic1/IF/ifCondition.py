1819cead24689ce653b339947ed525d21f03382d
Agus Sugiono <onowdev@gmail.com>
2022-02-07 10:40:18 +0700

    if 1 condition

---
+++ Basic1/IF/ifCondition.py
@@ -0,0 +1,22 @@
+'''
+Pendahuluan
+Ketika kita dihadapkan dengan pengambilan suatu keputusan tentunya kita akan mengevaluasi beberapa pilihan. 
+Di dalam bahasa pemrograman, pengambilan keputusan juga digunakan melalui penerapan konsep IF - ELSE. Konsep ini juga berlaku pada python yang memiliki kesederhanaan dalam penulisannya.
+
+Untuk itu, pada bab struktur kontrol: percabangan keputusan kamu akan mempelajari 3 bagian, yaitu:
+
+Menggunakan IF yang dapat dianalogikan dengan "Jika sesuatu memenuhi suatu kondisi maka lakukanlah A, namun jika tidak maka tidak terjadi apa-apa".
+Menggunakan IF dan ELSE yang dapat direpresentasikan dengan "Jika sesuatu memenuhi suatu kondisi maka lakukanlah A, namun jika tidak terpenuhi maka lakukanlah B".
+Menggunakan IF, ELIF dan ELSE. ELIF merupakan singkatan dari ELSE IF di python. Penerapan konstruksi IF, ELIF dan ELSE ini dapat dianalogikan dengan "Jika sesuatu memenuhi suatu kondisi maka lakukanlah A, jika tidak, lakukanlah pengecekan pada kondisi berikutnya yaitu jika memenuhi lakukanlah B, jika tidak maka lakukanlah C".
+Pada praktek operator IF, ELIF dan ELSE ini akan sedikit berbeda dengan sebelumnya. Perlu banyak modifikasi code yang dilakukan agar pemahaman materi bisa sempurna. Maka dari itu mohon dibaca dengan teliti ya!
+'''
+
+# contoh
+
+i = 9
+if (i == 9): 
+    print("Benar i bernilai 9") #Jika TRUE maka akan tercetak Kalimat ini.
+
+j = 70
+if(j == 90):
+    print("Salah j bukan 70 harusnya 90")
