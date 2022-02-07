defebf08db6db391a40dd55141d2aff813693dc2
Agus Sugiono <onowdev@gmail.com>
2022-02-07 10:21:26 +0700

    Add varian tipe data

--- Basic1/DataType.py
+++ Basic1/DataType.py
@@ -22,3 +22,38 @@ print(type("Hello world"))
 #tipe data Char
 
 print (type('A'))
+
+#tipe data Boolean
+print(True)
+
+
+#tipe data String
+print("Ayo belajar Python")
+print('Belajar Python Sangat Mudah di DQLAB')
+
+
+#tipe data Integer
+print(20)
+
+
+#tipe data Float
+print(3.14)
+
+
+#tipe data List
+print([1,2,3,4,5])
+print(["satu", "dua", "tiga"])
+
+
+#tipe data Tuple
+print((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+
+#tipe data Dictionary
+print({"nama":"Budi", 'umur':20})
+
+#tipe data Dictionary dimasukan ke dalam variabel biodata
+biodata = {"nama":"Andi", 'umur':21} #proses inisialisasi variabel biodata
+print(biodata) #proses pencetakan variabel biodata yang berisi tipe data Dictionary
+type(biodata) #fungsi untuk mengecek jenis tipe data. akan tampil <class 'dict'>
