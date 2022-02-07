02503876af184bed25fee4af66d1b3dddb0b7077
Agus Sugiono <onowdev@gmail.com>
2022-02-07 08:33:11 +0700

    tipedata

---
+++ Basic1/DataType.py
@@ -0,0 +1,24 @@
+# tipe Data Integer
+
+print(type(8))
+
+#tipe Data Float
+
+print(type(9.0))
+
+#Tipe Data Complex
+
+print(type(2 + 9j))
+
+#tipe data boolean
+
+print(type(True))
+print(type(False))
+
+#tipe data String
+
+print(type("Hello world"))
+
+#tipe data Char
+
+print (type('A'))
