d482ffe5bd2d27b83c16881dd3ff5f27c4100158
Agus Sugiono <onowdev@gmail.com>
2022-02-07 11:13:58 +0700

    modulus

---
+++ Basic1/OperasiMTK/Modulus.py
@@ -0,0 +1,14 @@
+'''
+Operasi modulus
+
+Modulus cukup jarang dipakai, akan tetapi untuk 
+beberapa kasus modulus sangat berguna terutama dalam 
+mempercepat proses perhitungan. Modulus merupakan 
+fungsi yang akan menghitung "sisa" dari hasil pembagian.
+modulus dilambangkan dengan %
+'''
+
+c = 90
+d = 40
+modulus= c % d
+print("Hasil 90 Modulus 40 adalah: ",modulus)
