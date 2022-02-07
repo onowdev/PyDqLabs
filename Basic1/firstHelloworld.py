34350b8808993a480ae87112a80ae36cc5999e30
Agus Sugiono <onowdev@gmail.com>
2022-02-07 08:20:37 +0700

    add multi comment

--- Basic1/firstHelloworld.py
+++ Basic1/firstHelloworld.py
@@ -1,3 +1,10 @@
 print("Hello Agus") # ini adalah String
 print('A') # ini Karakter
 print('B') 
+print("jadi # digunakan untuk membuat comment pada Python")
+
+# untuk menulis single line comment, atau 
+'''
+komentar ini
+lebih dari 1 baris
+''' 
