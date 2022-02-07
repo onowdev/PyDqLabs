60d9feb4b0de552a583e528776e1428342b720c8
Agus Sugiono <onowdev@gmail.com>
2022-02-07 12:56:41 +0700

    while 1

---
+++ Basic1/Perulangan/Looping/while.py
@@ -0,0 +1,16 @@
+'''
+while
+Struktur while pada python tidak berbeda jauh dengan 
+bahasa pemrograman lainnya. Lebih sederhananya, 
+struktur penulisan python membuat dia mudah untuk dibaca.
+'''
+
+
+j = 0 #nilai awal j =0
+
+
+while j<7: #ketika j kurang dari 7 lakukan perulangan, jika tidak stop perulangan
+    
+    print("Ini adalah perulangan ke -",j) #lakukan perintah ini ketika perulangan
+    
+    j=j+1 #setiap kali diakhir perulangan update nilai dengan ditambah 1.
