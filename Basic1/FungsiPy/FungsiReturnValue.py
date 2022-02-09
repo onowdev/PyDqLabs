f2adcfc8caa466d472c870489eb0875be2194323
onowdev <onowdev@gmail.com>
2022-02-09 13:46:22 +0700

    fungsi return value

---
+++ Basic1/FungsiPy/FungsiReturnValue.py
@@ -0,0 +1,18 @@
+def luasSegitiga(alas, tinggi):
+    luas = (alas * tinggi) / 2
+    return luas
+
+print("Luas Segitiga: %d" % luasSegitiga(4,6))
+
+'''
+Fungsi dengan Return Value
+Fungsi yang tidak mengembalikan nilai 
+biasanya disebut dengan prosedur. 
+Namun, kadang kita butuh hasil proses 
+dari fungsi untuk digunakan pada proses 
+berikutnya. Maka fungsi harus mengembalikan 
+nilai dari hasil pemrosesannya. 
+Cara mengembalikan nilai adalah menggunakan 
+kata kunci return lalu diikuti dengan nilai 
+atau variabel yang akan dikembalikan.
+'''
