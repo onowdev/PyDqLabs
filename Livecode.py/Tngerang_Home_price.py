df4280df66ec85e52d2b4ff3b6e02be31b801be9
onowdev <onowdev@gmail.com>
2022-02-16 14:27:10 +0700

    Make tangerang price House

---
+++ Livecode.py/Tngerang_Home_price.py
@@ -0,0 +1,10 @@
+import requests
+from contextlib import closing
+import csv
+# STEP 1: 
+# Baca file "harga_rumah.txt"
+url = "https://storage.googleapis.com/dqlab-dataset/harga_rumah.txt"
+data_harga_rumah = []
+with closing(requests.get(url, stream=True)) as r:
+    f = (line.decode('utf-8') for line in r.iter_lines())
+    data_harga_rumah = [row for row in csv.reader(f)]
