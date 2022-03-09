6e802d103c8039545e083fd620ff906deb66bbc0
onowdev <onowdev@gmail.com>
2022-03-09 20:48:17 +0700

    read hrg rumah

--- Latihan/Harga_Rumah.py
+++ Latihan/Harga_Rumah.py
@@ -1,4 +1,5 @@
 from urllib import response
+from attr import attributes
 import requests
 from contextlib import closing
 import csv
@@ -18,3 +19,32 @@ for baris_harga_rumah in data_harga_rumah[1:]:
 		dict_harga_rumah[key_harga_rumah[i]] = baris_harga_rumah[i]
 	harga_rumah.append(dict_harga_rumah)
 print(harga_rumah)
+# STEP 2:
+# Buat fungsi  get_all_specified_attribute yang menerima parameter list_of_dictionary 
+# (tipe data list yang berisikan sekumpulan tipe data dictionary) dan specified_key 
+# (tipe data string). Fungsi akan mengembalikan sebuah list yang berisikan seluruh 
+# atribut dengan kunci (key) specified_key. 
+def get_all_specified_attributes(list_of_dictionary, specified_key):
+	list_attributes = []
+	for data in list_of_dictionary:
+		attribute = data[specified_key]
+		list_attributes.append(attribute)
+	return list_attributes
+
+# STEP 3: 
+# Buat fungsi fungsi min_value yang menerima parameter list_attributes (berupa 
+# tipe data list) dan mengembalikan nilai terkecil dalam list_attributes 
+def min_value(list_attributes):
+	min_attribute = 9999
+	for attr in list_attributes:
+		if int(attr) < min_attribute:
+			min_attribute = int(attr)
+	return min_attribute
+# Buat fungsi dan max_value yang menerima parameter list_attribute dan 
+# mengembalikan nilai terbesar dalam list_attributes.	
+def max_value(list_attributes):
+	max_attribute = -9999
+	for attr in list_attributes:
+		if int(attr) > max_attribute:
+			max_attribute = int(attr)
+	return max_attribute
