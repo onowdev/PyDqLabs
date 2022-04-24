451a061ce5f30020b0ecf15fdc0df25bd0dcd49a
onowdev <onowdev@gmail.com>
2022-04-24 10:45:57 +0700

    add func

--- Basic1/FungsiPy/makeFunction.py
+++ Basic1/FungsiPy/makeFunction.py
@@ -2,4 +2,17 @@ def salam():
     print("Hello World")
     print("Selamat Pagi")
 
+def jawab():
+    print("Iya Hallo Juga")
+    print("Selamat Pagi Jugaa")
+
+def salam2():
+    print("siaapa Namamu")
+
+def jawab2():
+    print("Hallo waya Andra")
 salam()
+jawab()
+
+salam2()
+jawab2()
