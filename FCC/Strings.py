b7b0837e46126f9bfb8f2e1231e90d32cfaf0b98
onowdev <onowdev@gmail.com>
2022-05-02 20:39:59 +0700

    tipedata&Strings

---
+++ FCC/Strings.py
@@ -0,0 +1,6 @@
+first_name = 'Andra'
+last_name = 'Sugiono'
+midlle_name = 'alfarizqi'
+
+print(first_name+' '+midlle_name+' '+last_name)
+print(len(first_name+midlle_name+last_name))
---
+++ FCC/tipeData.py
@@ -0,0 +1,9 @@
+Nama = 'Andra'
+Usia = 5
+
+pi = 3.14
+
+mobil = ['BMW', 'Audi', 'Mercedes-Benz', ' bentley', 'Tesla-Motors']
+
+print(mobil)
+
